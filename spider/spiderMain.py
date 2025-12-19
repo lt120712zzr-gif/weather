55779e7b192f934f2bc99d98bee8ad5a1ff77d1a
陈佳特 <3416388153@qq.com>
2025-12-19 16:25:49 +0800

    12.19

--- spider/spiderMain.py
+++ spider/spiderMain.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from lxml import etree
 import os
@@ -43,3 +45,53 @@ class Spidermain():
     def main(self,url):
         respText = requests.get(url,headers=self.headers).text
         respXpath = etree.HTML(respText)
+        monthInfo = respXpath.xpath("//div[@class='inleft_tian']/ul[@class='tian_two']/li")
+        averageHeight = re.search('-?\d+',monthInfo[0].xpath(".//div[@class='tian_twoa']")[0].text).group()
+        averageSmall = re.search('-?\d+',monthInfo[0].xpath(".//div[@class='tian_twoa']")[1].text).group()
+        mastHeight = re.search('-?\d+',monthInfo[1].xpath(".//div[@class='tian_twoa']")[0].text).group()
+        try:
+            mastSmall = re.search('-?\d+',monthInfo[2].xpath(".//div[@class='tian_twoa']")[0].text).group()
+        except:
+            mastSmall = 0
+        averageAir = monthInfo[3].xpath(".//div[@class='tian_twoa']")[0].text
+        mastAir = monthInfo[4].xpath(".//div[@class='tian_twoa']")[0].text
+        lostAir = monthInfo[5].xpath(".//div[@class='tian_twoa']")[0].text
+
+        weatherList = respXpath.xpath("//ul[@class='thrui']/li")
+        for w in weatherList:
+            date = w.xpath("./div[@class='th200']")[0].text.split(' ')[0]
+            weekDay = w.xpath("./div[@class='th200']")[0].text.split(' ')[1]
+            try:
+                mastHeightDay = re.search('-?\d+', w.xpath("./div[@class='th140']")[0].text).group()
+            except:
+                mastHeightDay = 0
+            try:
+                mastSmallDay = re.search('-?\d+', w.xpath("./div[@class='th140']")[1].text).group()
+            except:
+                mastSmallDay = 0
+            try:
+                wearther =  w.xpath("./div[@class='th140']")[2].text
+            except:
+                wearther = '晴'
+            wind = w.xpath("./div[@class='th200']")[3].text.split(' ')[0]
+            try:
+               windOrder = re.search('-?\d+', w.xpath("./div[@class='th140']")[3].text.split(' ')[1]).group()
+            except:
+                windOrder = 3
+            self.writerRow([
+                averageHeight,
+                averageSmall,
+                mastHeight,
+                mastSmall,
+                averageAir,
+                mastAir,
+                lostAir,
+                date,
+                weekDay,
+                mastHeightDay,
+                mastSmallDay,
+                wearther,
+                wind,
+                windOrder,
+                self.city.replace('市','')
+            ])
