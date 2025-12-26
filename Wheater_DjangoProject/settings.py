bb67c6360ddaca94b8a758b2ac98e7821d72bad2
张志锐 <2803839598@qq.com>
2025-12-26 23:16:58 +0800

    12.26wan

--- Wheater_DjangoProject/settings.py
+++ Wheater_DjangoProject/settings.py
@@ -18,7 +18,6 @@ pymysql.version_info = (2, 2, 7, "final", 0)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-
+from models import *
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+def cityChar(request):
+    uname = request.session.get('username')
+    userInfo = User.objects.get(username=uname)
+    cites = getGlobalData()
+    city = request.GET.get('city') or cites[0]
+    print(city)
+    date, maxTemp, minTemp = getCityMinMaxTemp(city)
+    resultWeather = getWeatherListByCity(city)
+    resultWind = getWindListByCity(city)
+    resultWindOrder = getWindOrderListByCity(city)
+
+    return render(request, template_name='cityChar.html', context={
+        'userInfo': userInfo,
+        'cites': cites,
+        'defaultCity': city,
+        'date': date,
+        'maxTemp': maxTemp,
+        'minTemp': minTemp,
+        'resultWeather': resultWeather[:15],
+        'resultWind': resultWind,
+        'resultWindOrder': resultWindOrder,
+    })
